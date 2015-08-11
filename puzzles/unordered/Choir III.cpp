6c2d90bb272722d279ff45ce03a0544c04cf64d1
leic17 <740926583@qq.com>
2015-08-11 22:50:33 +0800

    Choir III accept in O(m*n^2) time complexity

--- puzzles/unordered/Choir III.cpp
+++ puzzles/unordered/Choir III.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 using namespace std;
 
-//time compelxity: O(m^2*n^2)
+//time compelxity: O(m*n^2)
 //space complexity: O(m*n)
-//Time limit exceeded
+//Wrong answer
 int choirChoose(vector<vector<int> > & choirVal, vector<vector<int> > & sexVal, int n, int m, int b, int g){
 	vector<vector<int> > sumVal(n, vector<int>(m));     //sumVal(i,j) means the sum of choir value in rectangle [0-i][0-j]
 	vector<vector<int> > sumBoy(n, vector<int>(m));     //sumBoy(i,j) means the sum  of boy in rectangle [0-i][0-j]
@@ -19,41 +19,20 @@ int choirChoose(vector<vector<int> > & choirVal, vector<vector<int> > & sexVal, 
 			int leftGirl, upGirl, leftUpGirl;
 			int leftNotAvail, upNotAvail, leftUpNotAvail;
 
-			if(i > 0){
-				leftVal = sumVal[i-1][j];
-				leftBoy = sumBoy[i-1][j];
-				leftGirl = sumGirl[i-1][j];
-				leftNotAvail = notAvail[i-1][j];
-			}else{
-				leftVal = 0;
-				leftBoy = 0;
-				leftGirl = 0;
-				leftNotAvail = 0;
-			}
+			upVal = i > 0 ? sumVal[i-1][j] : 0;
+			upBoy = i > 0 ? sumBoy[i-1][j] : 0;
+			upGirl = i > 0 ? sumGirl[i-1][j] : 0;
+			upNotAvail = i > 0 ? notAvail[i-1][j] : 0;
 
-			if(j > 0){
-				upVal = sumVal[i][j-1];
-				upBoy = sumBoy[i][j-1];
-				upGirl = sumGirl[i][j-1];
-				upNotAvail = notAvail[i][j-1];
-			}else{
-				upVal = 0;
-				upBoy = 0;
-				upGirl = 0;
-				upNotAvail = 0;
-			}
+			leftVal = j > 0 ? sumVal[i][j-1] : 0;
+			leftBoy = j > 0 ? sumBoy[i][j-1] : 0;
+			leftGirl = j > 0 ? sumGirl[i][j-1] : 0;
+			leftNotAvail = j > 0 ? notAvail[i][j-1] : 0;
 
-			if(i > 0 && j > 0){
-				leftUpVal = sumVal[i-1][j-1];
-				leftUpBoy = sumBoy[i-1][j-1];
-				leftUpGirl = sumGirl[i-1][j-1];
-				leftUpNotAvail = notAvail[i-1][j-1];
-			}else{
-				leftUpVal = 0;
-				leftUpBoy = 0;
-				leftUpGirl = 0;
-				leftUpNotAvail = 0;
-			}
+			leftUpVal = i > 0 && j > 0 ? sumVal[i-1][j-1] : 0;
+			leftUpBoy = i > 0 && j > 0 ? sumBoy[i-1][j-1] : 0;
+			leftUpGirl = i > 0 && j > 0 ? sumGirl[i-1][j-1] : 0;
+			leftUpNotAvail = i > 0 && j > 0 ? notAvail[i-1][j-1] : 0;
 
 			sumVal[i][j] = leftVal + upVal - leftUpVal + choirVal[i][j];
 			sumBoy[i][j] = leftBoy + upBoy - leftUpBoy + (sexVal[i][j] == 1 ? 1 : 0);
@@ -63,68 +42,48 @@ int choirChoose(vector<vector<int> > & choirVal, vector<vector<int> > & sexVal, 
 	}
 
 	int res = -1;
-	int tempSum, tempBoy, tempGirl, tempNotAvail;
+	int tempSum, tempBoy, tempGirl, colNotAvail;
 
-	for(int i = 0; i < n; i++){
-		for(int ii = i; ii < n; ii++){
-			for(int j = 0; j < m; j++){
-				for(int jj = j; jj < m; jj++){
-					int leftVal, upVal, leftUpVal;
-					int leftBoy, upBoy, leftUpBoy;
-					int leftGirl, upGirl, leftUpGirl;
-					int leftNotAvail, upNotAvail, leftUpNotAvail;
+	int i, ii, j, jj;
+	for(i = 0; i < n; i++){
+		for(ii = i; ii < n; ii++){
+			for(j = 0, jj = j; jj < m; jj++){
+				int leftVal, upVal, leftUpVal;
+				int leftBoy, upBoy, leftUpBoy;
+				int leftGirl, upGirl, leftUpGirl;
+				int leftColNotAvail, upColNotAvail, leftUpColNotAvail;
 
-					if(i > 0){
-						leftVal = sumVal[i-1][j];
-						leftBoy = sumBoy[i-1][j];
-						leftGirl = sumGirl[i-1][j];
-						leftNotAvail = notAvail[i-1][j];
-					}else{
-						leftVal = 0;
-						leftBoy = 0;
-						leftGirl = 0;
-						leftNotAvail = 0;
-					}
+				upVal = i > 0 ? sumVal[i-1][jj] : 0;
+				upBoy = i > 0 ? sumBoy[i-1][jj] : 0;
+				upGirl = i > 0 ? sumGirl[i-1][jj] : 0;
+				upColNotAvail = i > 0 ? notAvail[i-1][jj] : 0;
 
-					if(j > 0){
-						upVal = sumVal[i][j-1];
-						upBoy = sumBoy[i][j-1];
-						upGirl = sumGirl[i][j-1];
-						upNotAvail = notAvail[i][j-1];
-					}else{
-						upVal = 0;
-						upBoy = 0;
-						upGirl = 0;
-						upNotAvail = 0;
-					}
+				leftVal = j > 0 ? sumVal[ii][j-1] : 0;
+				leftBoy = j > 0 ? sumBoy[ii][j-1] : 0;
+				leftGirl = j > 0 ? sumGirl[ii][j-1] : 0;
+				leftColNotAvail = jj > 0 ? notAvail[ii][jj-1] : 0;
 
-					if(i > 0 && j > 0){
-						leftUpVal = sumVal[i-1][j-1];
-						leftUpBoy = sumBoy[i-1][j-1];
-						leftUpGirl = sumGirl[i-1][j-1];
-						leftUpNotAvail = notAvail[i-1][j-1];
-					}else{
-						leftUpVal = 0;
-						leftUpBoy = 0;
-						leftUpGirl = 0;
-						leftUpNotAvail = 0;
-					}
+				leftUpVal = i > 0 && j > 0 ? sumVal[i-1][j-1] : 0;
+				leftUpBoy = i > 0 && j > 0 ? sumBoy[i-1][j-1] : 0;
+				leftUpGirl = i > 0 && j > 0 ? sumGirl[i-1][j-1] : 0;
+				leftUpColNotAvail = i > 0 && jj > 0 ? notAvail[i-1][jj-1] : 0;
 
-					tempSum = sumVal[ii][jj] + leftUpVal - leftVal - upVal;
-					tempBoy = sumBoy[ii][jj] + leftUpBoy - leftBoy - upBoy;
-					tempGirl = sumGirl[ii][jj] + leftUpGirl - leftGirl - upGirl;
-					tempNotAvail = notAvail[ii][jj] + leftUpNotAvail - leftNotAvail - upNotAvail;
+				tempSum = sumVal[ii][jj] + leftUpVal - leftVal - upVal;
+				tempBoy = sumBoy[ii][jj] + leftUpBoy - leftBoy - upBoy;
+				tempGirl = sumGirl[ii][jj] + leftUpGirl - leftGirl - upGirl;
+				colNotAvail = notAvail[ii][jj] + leftUpColNotAvail - leftColNotAvail - upColNotAvail;
 
-					if(tempBoy < b || tempGirl < g){
-						continue;
-					}
-
-					if(tempNotAvail > 0)
-						continue;
-
-					if(tempSum > res)
-						res = tempSum;
+				if(colNotAvail > 0){
+					j = jj + 1;
+					continue;
 				}
+
+				if(tempBoy < b || tempGirl < g){
+					continue;
+				}
+
+				if(tempSum > res)
+					res = tempSum;
 			}
 		}
 	}
