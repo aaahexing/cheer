28146b49582a51e80f44eb4d8955e7c1942edcdd
leic17 <740926583@qq.com>
2015-10-05 09:32:22 +0800

    add an universal solution to leetcode 80
    RT

--- puzzles/leetcode/80.cpp
+++ puzzles/leetcode/80.cpp
@@ -8,6 +8,7 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 **/
 
+//-------------------------------solution 1-----------------------------
 //@time complexity: O(n)
 //@space complexity: O(1)
 //@2A
@@ -34,5 +35,42 @@ int removeDuplicates(vector<int>& nums) {
     }
         
     nums[pos++] = nums[cur];
+    return pos;
+}
+
+//-------------------------------------solution 2---------------------------------
+//@universal solution
+//@time complexity: O(n)
+//@space complexity: O(1)
+//@2A
+int removeDuplicates(vector<int>& nums) {
+    int pos = 0, cur = 0, next = 1, countDup = 1;
+    if (nums.size() <= 2) {
+        return nums.size();
+    }
+        
+    while(cur < nums.size() && next < nums.size()) {
+        if (nums[cur] == nums[next]) {
+            countDup++;
+            next++;
+        } else {
+            int count = 0;
+            while (countDup > 0 && count < 2) {
+                nums[pos++] = nums[cur];
+                countDup--;
+                count++;
+            }
+            cur = next;
+            next = next + 1;
+            countDup = 1;
+        }
+    }
+        
+    int count = 0;
+    while (countDup > 0 && count < 2) {
+        nums[pos++] = nums[cur];
+        countDup--;
+        count++;
+    }
     return pos;
 }
