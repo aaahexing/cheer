54737eb70b84b91b5514975b0985be13d75afa08
leic17 <740926583@qq.com>
2015-10-18 19:18:46 +0800

    leetcode 39,40 commitment
    RT

---
+++ puzzles/leetcode/39.cpp
@@ -0,0 +1,47 @@
+﻿/**************************************
+Combination Sum : https://leetcode.com/problems/combination-sum/
+
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T. 
+The same repeated number may be chosen from C unlimited number of times. 
+Note:
+•All numbers (including target) will be positive integers.
+•Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+•The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+ A solution set is: 
+[7] 
+[2, 2, 3] 
+**/
+
+//@backtracking
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > ret;
+        vector<int> cur;
+        combinationSum(ret, candidates, cur, 0, candidates.size() - 1, target);
+        return ret;
+    }
+    
+    void combinationSum(vector<vector<int> > & ret, vector<int>& candidates, vector<int>& cur, int start, int end, int target) {
+        if (target == 0) {
+            ret.push_back(cur);
+            return;
+        } else if (start > end) {
+            return;
+        }
+        
+        for (int i = start; i <= end; i++) {
+            for (int j = 1; j <= target / candidates[i]; j++) {
+                if (target - j * candidates[i] >= 0) {
+                    cur.push_back(candidates[i]);
+                    combinationSum(ret, candidates, cur, i+1, end, target-candidates[i] * j);
+                }
+            }
+            for (int j = 1; j <= target / candidates[i]; j++) {
+                cur.pop_back();
+            }
+        }
+    }
+};
---
+++ puzzles/leetcode/40.cpp
@@ -0,0 +1,51 @@
+﻿/*********************************
+Combination Sum II : https://leetcode.com/problems/combination-sum-ii/
+
+Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T. 
+Each number in C may only be used once in the combination. 
+Note:
+•All numbers (including target) will be positive integers.
+•Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+•The solution set must not contain duplicate combinations.
+For example, given candidate set 10,1,2,7,6,1,5 and target 8, 
+ A solution set is: 
+[1, 7] 
+[1, 2, 5] 
+[2, 6] 
+[1, 1, 6] 
+**/
+
+//@space complexity: O(n)
+//@2A
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > ret;
+        vector<int> cur;
+        combinationSum2(ret, candidates, cur, 0, candidates.size() - 1, target);
+        return ret;
+    }
+    
+    void combinationSum2(vector<vector<int> > & ret, vector<int>& candidates, vector<int>& cur, int start, int end, int target) {
+        if (target == 0) {
+            ret.push_back(cur);
+            return;
+        } else if (start > end) {
+            return;
+        }
+        
+        for (int i = start; i <= end; i++) {
+            if (target - candidates[i] >= 0) {
+				if (i > start && candidates[i] == candidates[i - 1]) {
+					continue;
+				}
+                cur.push_back(candidates[i]);
+                combinationSum2(ret, candidates, cur, i+1, end, target-candidates[i]);
+                cur.pop_back();
+            } else {
+                break;
+            }
+        }
+    }
+};
